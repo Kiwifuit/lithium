ae15d3cc54f8850663ba41cc2a2a676eb5667387
Kiwifuit <mahkiwi123@gmail.com>
2022-11-20 21:53:12 +0800

    fix: Small tweaks to DatabaseError

--- src/err.rs
+++ src/err.rs
@@ -1,18 +1,17 @@
 use std::fmt::Display;
 
 pub enum DatabaseError {
-    UsernameReadError,
-    PasswordReadError,
+    UsernameQueryPrepareError,
 }
 
 impl Display for DatabaseError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "An unknown error occurred while reading the {} value in the database",
+            "{}",
             match self {
-                DatabaseError::PasswordReadError => "password",
-                DatabaseError::UsernameReadError => "username",
+                Self::UsernameQueryPrepareError =>
+                    "An unknown error occurred while preparing to query the database",
             }
         )
     }

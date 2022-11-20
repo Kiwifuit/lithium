1147765fe9d562d5fa68cc167631b176b3f505a5
Kiwifuit <mahkiwi123@gmail.com>
2022-11-20 21:48:56 +0800

    impl: Error type for querying the database

--- src/err.rs
+++ src/err.rs
@@ -1,1 +1,19 @@
+use std::fmt::Display;
 
+pub enum DatabaseError {
+    UsernameReadError,
+    PasswordReadError,
+}
+
+impl Display for DatabaseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "An unknown error occurred while reading the {} value in the database",
+            match self {
+                DatabaseError::PasswordReadError => "password",
+                DatabaseError::UsernameReadError => "username",
+            }
+        )
+    }
+}

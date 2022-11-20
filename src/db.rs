85a40bb5b011f5c3a31d378795292bc70f0f7f0c
Kiwifuit <mahkiwi123@gmail.com>
2022-11-20 21:57:57 +0800

    impl: Login struct that will store the logins

--- src/db.rs
+++ src/db.rs
@@ -1,1 +1,17 @@
+pub struct Login {
+    username: String,
+    password: Option<String>,
+}
 
+impl Login {
+    pub fn new(username: &str) -> Self {
+        Self {
+            username: username.to_string(),
+            password: None,
+        }
+    }
+
+    pub fn update_password(&mut self, password: &str) {
+        self.password = Some(password.to_string())
+    }
+}

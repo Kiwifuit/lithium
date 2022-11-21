2a191b6d363c33992db8981a230a5d664f4cf697
Kiwifuit <mahkiwi123@gmail.com>
2022-11-21 08:19:24 +0800

    fix: Added debug trait for DatabaseError

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 mod db;
 mod err;
 
+use rusqlite::Connection;
+
 fn main() {
-    println!("Hello, world!");
+    let conn = Connection::open("./data/Profile 1/Login Data").unwrap();
+
+    for mut username in db::query_all_usernames(&conn).unwrap() {
+        if let Err(e) = db::query_password_for(&conn, &mut username) {
+            println!("An error occurred while querying the database:\n\t{}", e);
+            continue;
+        }
+
+        println!("Login {:?}", username);
+    }
 }

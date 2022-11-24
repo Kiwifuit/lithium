5970c5f29a68c90778dc1f4fd94a4d7c3d8b68e1
Kiwifuit <mahkiwi123@gmail.com>
2022-11-24 22:56:56 +0800

    impl: Added `encryption_key` variable in main program Yep, thats all

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,14 @@ mod err;
 mod path;
 
 fn main() {
+    let encryption_key = match path::get_encryption_key() {
+        Ok(k) => k,
+        Err(e) => {
+            eprintln!("An error occurred while getting the encryption key: {}", e);
+            exit(10)
+        }
+    };
+
     let profiles = match path::get_all_profiles() {
         Ok(i) => i,
         Err(e) => panic!("An error occurred while getting profiles: {}", e),
